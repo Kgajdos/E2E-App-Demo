1884e339107e3611aab1fa7777c8f28485d1c60e
Kevin <96949155+Kgajdos@users.noreply.github.com>
2023-12-22 12:23:45 -0500

    Started the process of adding new views to web page.

--- E2E App Demo/Controllers/HelloWorldController.cs
+++ E2E App Demo/Controllers/HelloWorldController.cs
@@ -5,17 +5,19 @@ namespace E2E_App_Demo.Controllers
 {
 	public class HelloWorldController : Controller
 	{
-		//
-		//GET: /HelloWorld/
-		public string Index()
-		{
-			return "This is my default action...";
-		}
-		//
-		//GET: /HelloWorld/Welcome/
-		public string Welcome(string name, int ID = 1)
-		{
-			return HtmlEncoder.Default.Encode($"Hello, {name}, ID: {ID}");
-		}
-	}
+        //
+        //GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //
+        //GET: /HelloWorld/Welcome/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
 }
---
+++ E2E App Demo/Pages/Welcome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E2E_App_Demo.Pages
+{
+    public class WelcomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
